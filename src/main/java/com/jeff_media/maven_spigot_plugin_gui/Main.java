a99dc2a1e9959aae716d44bc72d9c49f7c2f4dad
mfnalex <1122571+mfnalex@users.noreply.github.com>
2023-01-17 02:04:55 +0100

    Working, next changing to GridBagLayout

---
+++ src/main/java/com/jeff_media/maven_spigot_plugin_gui/Main.java
@@ -0,0 +1,13 @@
+package com.jeff_media.maven_spigot_plugin_gui;
+
+import lombok.Getter;
+import org.apache.log4j.Logger;
+
+import javax.swing.*;
+
+public class Main {
+
+    @Getter
+    private static final Logger logger = Logger.getLogger(Main.class);
+
+}
---
+++ src/main/java/com/jeff_media/maven_spigot_plugin_gui/TestForm.java
@@ -0,0 +1,8 @@
+package com.jeff_media.maven_spigot_plugin_gui;
+
+import javax.swing.*;
+
+public class TestForm {
+    private JTextField textField1;
+    private JTextField textField2;
+}
